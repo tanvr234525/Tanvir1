7a3f5d5e0cc5e6903a852256992fbb0007a9afc4
Mohammad Rahad <126962816+MR-RAHAD-511@users.noreply.github.com>
2024-02-07 20:28:02 +0600

    Update ig5.js

--- scripts/commands/ig5.js
+++ scripts/commands/ig5.js
@@ -1,6 +1,6 @@
 /** Đổi Credit ? Bọn t đã không mã hóa cho mà edit rồi thì tôn trọng nhau tý đi ¯\_(ツ)_/¯ **/
 module.exports.config = {
-  name: "ig1",
+  name: "ig2",
   version: "1.0.0", 
   permission: 0,
   credits: "nayan",
