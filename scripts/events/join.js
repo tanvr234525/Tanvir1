b2c3b56a40b96110675f37a64b8be0e9898acd16
Mohammad Rahad <126962816+MR-RAHAD-511@users.noreply.github.com>
2024-02-07 18:44:54 +0600

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -48,7 +48,7 @@ module.exports.run = async function({ api, event, Users }) {
   if (event.logMessageData.addedParticipants && Array.isArray(event.logMessageData.addedParticipants) && event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
     //api.changeNickname(`𝗕𝗢𝗧 ${(!global.config.BOTNAME) ? "Buddy" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
 
-    let gifUrl = 'https://i.postimg.cc/SNQXkB0y/lv-0-20231018174834.gif';
+    let gifUrl = 'https://drive.google.com/uc?id=1QrSCgq16zgsLddZFyM6Tj-9W2iW5psVT';
 let gifPath = __dirname + '/Nayan/join/join.gif';
 
 axios.get(gifUrl, { responseType: 'arraybuffer' })
