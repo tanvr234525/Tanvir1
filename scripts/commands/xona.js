587ccdb7f8d37dd0b01e2538c3aa96fd89d5c93c
Mohammad Rahad <126962816+MR-RAHAD-511@users.noreply.github.com>
2024-02-09 23:13:21 +0600

    Create xona.js

---
+++ scripts/commands/xona.js
@@ -0,0 +1,23 @@
+module.exports.config = {
+  name: "ig",
+  version: "1.0.0", 
+  permission: 0,
+  credits: "Mohammad Rahad",
+  description: "", 
+  prefix: true,
+  category: "user",
+  usages: "",
+  cooldowns: 5, 
+  dependencies: {
+	}
+};
+
+
+module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+   var hi = ["🥵🥵🥵"
+ ];
+  var know = hi[Math.floor(Math.random() * hi.length)];
+    var link = [
