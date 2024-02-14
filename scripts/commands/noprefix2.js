338f57bcf55fd7f6254cd60e5b1402d42cf6a528
Mohammad Rahad <126962816+MR-RAHAD-511@users.noreply.github.com>
2024-02-14 13:02:03 +0600

    Update noprefix2.js

--- scripts/commands/noprefix2.js
+++ scripts/commands/noprefix2.js
@@ -16,11 +16,11 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("👨‍❤️‍💋‍👨")==0 || event.body.indexOf("😘")==0 || event.body.indexOf("😽")==0 || event.body.indexOf("🙁")==0) {
 		var msg = {
-				body: "--চাইলেই যদি ভোলা যেতো...!\nতাহলে সুসাইড বলে কিছুই থাকত না..!🙂\n𝗠𝗼𝗵𝗮𝗺𝗺𝗮𝗱 𝗥𝗮𝗵𝗮𝗱 🙂",
+				body: "- আমি চাই তুমি একান্তই আমার হও, একান্তই আমার 🤎🧡\n𝗠𝗼𝗵𝗮𝗺𝗺𝗮𝗱 𝗥𝗮𝗵𝗮𝗱 😻",
 				attachment: fs.createReadStream(__dirname + `/noprefix/rahad10.mp4`)
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("🙂", event.messageID, (err) => {}, true)
+    api.setMessageReaction("😻", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
