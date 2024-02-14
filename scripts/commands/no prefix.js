2eb25e2ce356a57f86db3a8e19d8464a77fff67e
Mohammad Rahad <126962816+MR-RAHAD-511@users.noreply.github.com>
2024-02-14 13:09:59 +0600

    Update no prefix.js

--- scripts/commands/no prefix.js
+++ scripts/commands/no prefix.js
@@ -1,27 +1,28 @@
+const fs = require("fs");
 module.exports.config = {
-  name: "😘",
+  name: "😍",
   version: "1.0.0", 
   permission: 0,
   credits: "Rahad",
   description: "", 
   prefix: true,
-  category: "😘",
+  category: "user",
   usages: "",
   cooldowns: 5, 
- 
+  
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("🖤")==0 || event.body.indexOf("😍")==0 || event.body.indexOf("❤️")==0 || event.body.indexOf("👩‍❤️‍👨")==0) {
+	if (event.body.indexOf("😍")==0 || event.body.indexOf("🖤")==0 || event.body.indexOf("💑")==0 || event.body.indexOf("🫂")==0) {
 		var msg = {
 				body: "         KINNA SONA🖤 \n\n𝗠𝗼𝗵𝗮𝗺𝗺𝗮𝗱 𝗥𝗮𝗵𝗮𝗱👑",
 				attachment: fs.createReadStream(__dirname + `/noprefix/rahad19.mp4`)
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("😍", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🙂", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-  }
+	}
