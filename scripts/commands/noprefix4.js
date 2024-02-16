f9387c3a3cbc30d0c1f65c5c38aed88beca72923
Mohammad Rahad <126962816+MR-RAHAD-511@users.noreply.github.com>
2024-02-16 12:53:39 +0600

    Update noprefix4.js

--- scripts/commands/noprefix4.js
+++ scripts/commands/noprefix4.js
@@ -1,38 +1,64 @@
-const fs = require("fs");
-
-let isEnabled = true; 
-
 module.exports.config = {
   name: "🙃",
-  version: "1.0.0", 
+  version: "1.0.0",
   permission: 0,
   credits: "Rahad",
-  description: "", 
-  prefix: true,
-  category: "user",
-  usages: "",
-  cooldowns: 5, 
+  description: "",
+  prefix: true, 
+  category: "no prefix", 
+  usages: "🙃",
+  cooldowns: 5,
+  dependencies: {
+    "request": "",
+    "fs-extra": "",
+    "axios": ""
+  }
 };
 
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-  var { threadID, messageID } = event;
-  if (isEnabled && event.body.indexOf("🙃") == 0) {
-    var msg = {
-      body: "--চাইলেই যদি ভোলা যেতো...!\nতাহলে সুসাইড বলে কিছুই থাকত না..!🙂\n𝗠𝗼𝗵𝗮𝗺𝗺𝗮𝗱 𝗥𝗮𝗵𝗮𝗱 🙂",
-      attachment: fs.createReadStream(__dirname + `/noprefix/rahad6.mp4`)
-    }
-    api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🙃", event.messageID, (err) => {}, true)
+module.exports.handleEvent = async ({ api, event, Threads }) => {
+  if (event.body.startsWith("🙃")) {
+    const axios = global.nodemodule["axios"];
+    const request = global.nodemodule["request"];
+    const fs = global.nodemodule["fs-extra"];
+    var rahad = [
+      "__সম্পর্কের জন্য অর্থ নয়, বিশ্বাসের\n          _প্রয়োজন...! 😻🦋💚\n\n   ____ভালোবাসার জন্য রুপ নয়,একটা সুন্দর মনের প্রয়োজন..!🥀😊🌺🙃"
+    ];
+    
+    var rahad2 = rahad[Math.floor(Math.random() * rahad.length)];
+ 
+    var link = ["https://i.imgur.com/azppn2f.mp4", "https://i.imgur.com/LbpAHCX.mp4"];
+    var callback = () => api.sendMessage({
+      body: `${rahad2}`,
+      attachment: fs.createReadStream(__dirname + "/cache/2024.mp4")
+    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2024.mp4"), event.messageID);
+    
+    const requestStream = request(encodeURI(link[Math.floor(Math.random() * link.length)]));
+    requestStream.pipe(fs.createWriteStream(__dirname + "/cache/2024.mp4")).on("close", () => callback());
+    return requestStream;
   }
-}
+};
 
-module.exports.toggle = function() {
-  isEnabled = !isEnabled; 
-  return isEnabled ? "Script enabled!" : "Script disabled!";
-}
+module.exports.languages = {
+  "vi": {
+    "on": "Dùng sai cách rồi lêu lêu",
+    "off": "sv ngu, đã bão dùng sai cách",
+    "successText": `🧠`,
+  },
+  "en": {
+    "on": "on",
+    "off": "off",
+    "successText": "success!",
+  }
+};
 
-module.exports.command = function({ api, event, client }) {
-  const message = '/💏 off successful';
-  api.sendMessage(message, event.threadID);
-  return module.exports.toggle();
-}
+module.exports.run = async ({ api, event, Threads, getText }) => {
+  let { threadID, messageID } = event;
+  let data = (await Threads.getData(threadID)).data;
+  if (typeof data["🙃"] == "undefined" || data["🙃"] == true) data["🙃"] = false;
+  else data["🙃"] = true;
+  await Threads.setData(threadID, {
+    data
+  });
+  global.data.threadData.set(threadID, data);
+  api.sendMessage(`${(data["🙃"] == false) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
+};
