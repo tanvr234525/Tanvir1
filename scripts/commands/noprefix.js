5934ba04e44bf17c739363c7ab495003c558fb4f
joyahmed017 <167390635+joyahmed017@users.noreply.github.com>
2024-04-18 16:06:57 +0600

    Update noprefix.js

--- scripts/commands/noprefix.js
+++ scripts/commands/noprefix.js
@@ -16,7 +16,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("😍")==0 || event.body.indexOf("🖤")==0 || event.body.indexOf("💑")==0 || event.body.indexOf("🫂")==0) {
 		var msg = {
-				body: "         KINNA SONA🖤 \n\n𝗠𝗼𝗵𝗮𝗺𝗺𝗮𝗱 𝗥𝗮𝗵𝗮𝗱👑",
+				body: "         KINNA SONA🖤 \n\n𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐌𝐄𝐃 𝐉𝐎𝐘👑",
 				attachment: fs.createReadStream(__dirname + `/noprefix/Rahad19.mp4`)
 			}
 			api.sendMessage( msg, threadID, messageID);
